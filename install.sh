697f74aefba9ca18e5cc091153f71851642b24fe
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-07-17 13:25:13 +0800

    added vhost config template in install.sh comment

--- install.sh
+++ install.sh
@@ -1,25 +1,50 @@
 #!/usr/bin/env bash
 
-## download swagger-editor
-#git clone https://github.com/swagger-api/swagger-editor.git seed
+## requirement
+# assume the apache document root is /var/www
+# require to enable symbolic under the vhost
+# require the /var/www/api_doc link to this folder (the working directory of this install.sh)
 #
-## build swagger-editor
-#cd seed
-#res=$(npm install && npm run build)
-#if [ res != 0 ]; then
-#  echo "failed to build swagger-editor, please update the npm and node(js)";
-#  exit 1;
-#fi
-#cd ../
+# the vhost conf file should looks like this:
+# ```
+# <VirtualHost *:8081>
+#         #ServerName www.example.com
 #
-## clean up swagger-editor
+#         ServerAdmin webmaster@localhost
+#         DocumentRoot /var/www/api_doc
+#
+#         #LogLevel info ssl:warn
+#
+#         ErrorLog ${APACHE_LOG_DIR}/error.log
+#         CustomLog ${APACHE_LOG_DIR}/access.log combined
+#
+#         #Include conf-available/serve-cgi-bin.conf
+#
+#         <Directory /var/www/api_doc/>
+#                 Options Indexes FollowSymLinks    # the folder will appear after login (first time need to know the folder name (full url) externally )
+#                 #Options FollowSymLinks
+#                 #AllowOverride None
+#                 AllowOverride AuthConfig
+#                 #Require all granted
+#                 Order allow,deny
+#                 Allow from all
+#         </Directory>
+#
+# </VirtualHost>
+# ```
 
 
-# download swagger-editor bundle
+# download swagger-editor bundle as seed project
 wget https://github.com/swagger-api/swagger-editor/releases/download/v2.10.3/swagger-editor.zip
 unzip swagger-editor.zip
 rm swagger-editor.zip
 mv swagger-editor seed
 
+# set up seed project
+cd seed;
+echo "AuthType Basic"                                    > .htaccess
+echo "AuthName \"Authentication Required\""             >> .htaccess
+echo "AuthUserFile \"/var/www/api_doc/seed/.htpasswd\"" >> .htaccess
+echo "Require valid-user"                               >> .htaccess
 
-# set project
+echo "demo:\$apr1$JJuyRiXp\$0ZFmCMPNzaDORrWFM098e1"      > .htpasswd
